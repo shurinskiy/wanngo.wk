fca837fc22e07e86e6dc12e6618903962efbf49d
_tango <shurinskiy@mail.ru>
2021-08-13 18:16:24 +0600

    search behavior

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,4 +1,23 @@
 (() => {
+	let $search = $('.header__search');
 
+	$search.on('click', function(e) {
+		let $self = $(this);
+
+		if(parseInt($self.css('border')) < 40) {
+			$self
+				.not('.opened')
+				.addClass('opened')
+				.find('input[type="text"]')
+				.trigger('focus');
+		}
+	});
+
+	$(window).on('click', function(e) {
+		if($search.hasClass('opened') && !e.target.closest('.header__search')) {
+			e.preventDefault();
+			$search.removeClass('opened');
+		}
+	});
 
 })();
