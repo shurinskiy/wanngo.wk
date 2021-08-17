ac6a771363b9ac23d95d767d17e925d21e8340ba
_tango <shurinskiy@mail.ru>
2021-08-17 21:00:47 +0600

    before filter adaptive

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,4 +1,8 @@
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
+
 (() => {
+	let $shell = $('.header__navi');
+	let $toggle = $('.header__toggle');
 	let $search = $('.header__search');
 
 	$search.on('click', function(e) {
@@ -13,10 +17,32 @@
 		}
 	});
 
+	$toggle.on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		$shell.toggleClass('opened');
+		$(this).toggleClass('opened');
+
+		if($shell.hasClass('opened')) {
+			disablePageScroll();
+			return;
+		}
+
+		enablePageScroll();
+	});
+
 	$(window).on('click', function(e) {
 		if($search.hasClass('opened') && !e.target.closest('.header__search')) {
 			e.preventDefault();
 			$search.removeClass('opened');
+		}
+
+		if(($shell.hasClass('opened') && !e.target.closest('.header__navi')) || e.which == 27) {
+			e.preventDefault();
+			$toggle.removeClass('opened');
+			$shell.toggleClass('opened');
+			enablePageScroll();
 		}
 	});
 })();
