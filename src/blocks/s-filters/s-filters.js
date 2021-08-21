71d397efb77762dc06316597045df89f94f21125
_tango <shurinskiy@mail.ru>
2021-08-21 18:42:33 +0600

    filter selected fix

--- src/blocks/s-filters/s-filters.js
+++ src/blocks/s-filters/s-filters.js
@@ -9,9 +9,29 @@
 	$('.s-filters__block').each(function() {
 		let $self = $(this);
 		let $count = $self.find('.s-filters__title-selected');
+		let $checks = $self.find('.s-filters__item input[type="checkbox"]');
 
-		$self.on('click', 's-filters__item checkbox', function(e) {
-			
+		$self.on('change', $checks, function(e) {
+			let ids = [];
+
+			$checks.each(function(index) {
+				if ($(this).is(':checked')) {
+					ids.push(index);
+				}
+			});
+
+			if($.isEmptyObject(ids)) {
+				$count
+					.removeAttr('style')
+					.find('span')
+					.empty();
+			} else {
+				$count
+					.show()
+					.find('span')
+					.text(ids.length);
+			}
+
 		});
 	});
 
